ed96a8bd3dd6bbe1f1abcbfa8505e2966a1e03a8
Abrar Hussain <117599985+sahussain887@users.noreply.github.com>
2023-10-06 19:16:01 -0500

    update

---
+++ friendify/client/components/UserProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserProfile = () => {
+  return (
+    <div>Abrar will work on this component </div>
+  )
+}
+
+export default UserProfile
---
+++ friendify/client/pages/profile.js
@@ -0,0 +1,14 @@
+import Navbar from '@/components/Navbar'
+import UserProfile from '@/components/UserProfile'
+import React from 'react'
+
+const profile = () => {
+  return (
+    <div>
+        <Navbar/>
+        <UserProfile/>
+    </div>
+  )
+}
+
+export default profile
